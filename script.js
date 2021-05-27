b6b62f814a1ae78b3d64cbb11db576fcbeb49fed
Vanapalli Naveen Kumar <naveen.vanapalli@forsys.taas.com>
2021-05-27 20:11:48 +0530

    Inital Commit

---
+++ script.js
@@ -0,0 +1,54 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const quoteAuthor = document.getElementById("author");
+const quoteTwitter = document.getElementById("twitter");
+const quoteNewQuote = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
+let apiQuotes = [];
+
+function loading(){
+    loader.classList.add('show');
+    quoteContainer.hidden = true;
+}
+
+function complete(){
+    quoteContainer.hidden = false;
+    loader.classList.remove('show');
+}
+// Show New Quote
+function newQuote(){
+    loading();
+    // Pick Ramdon quote from ApiQuotes array
+    const quote= apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    quoteAuthor.textContent = quote.author ? quote.author : "Naveen";
+    if(quote.text.length > 50){
+        quoteText.classList.add('long-quote');
+    }else{
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = quote.text ? quote.text : "Quote was missing, Try to get new quote";
+    complete();
+  }
+// Get Quote From API
+async function getQuotes(){
+    loading();
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        alert(error);
+        // Catch Error Here
+    }
+}
+function tweetQuote(){
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${quoteAuthor.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+quoteNewQuote.addEventListener('click',newQuote);
+quoteTwitter.addEventListener('click',tweetQuote);
+getQuotes();
